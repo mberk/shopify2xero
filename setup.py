b98848bd2d410e6695cd83306996ddd09c0f128a
Maurice Berk <maurice@mauriceberk.com>
2020-11-11 05:52:55 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='shopify2xero',
+    version='0.0.1',
+    description='A Python package for exporting Shopify data and importing it to Xero. For example, export Shopify orders and import them as Xero invoices ',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Maurice Berk',
+    author_email='maurice@mauriceberk.com',
+    url='https://github.com/mberk/shopify2xero',
+    packages=['shopify2xero'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.5',
+)
